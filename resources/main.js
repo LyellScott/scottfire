b83705846b1800387f62667c6c00f0842b646226
LyellScott <lyellscott@outlook.com>
2018-02-09 12:35:08 +1000

    Added Jquery for form

--- resources/main.js
+++ resources/main.js
@@ -1,32 +1,45 @@
 $(document).ready (function() {
+    $('.training-modules').on('click',function() {
+        $('form').removeClass("dimmerSwitch");
+    });
     $('.one').on('click',function() {
         var currentTot = +$('#totPrice').text();
         var addedItem = +$('#extPrice').text();
         var $newTotal = ((addedItem + currentTot));
-    var $newPrice = $('<span>' + $newTotal + '</span>');
+    var $newPrice = $('<span id="totPrice">' + $newTotal + '</span>');
     $('#totPrice').replaceWith($newPrice);
     });
     $('.two').on('click',function() {
         var currentTot = +$('#totPrice').text();
         var addedItem = +$('#wardenPrice').text();
         var $newTotal = ((addedItem + currentTot));
-    var $newPrice = $('<span>' + $newTotal + '</span>');
+    var $newPrice = $('<span id="totPrice">' + $newTotal + '</span>');
     $('#totPrice').replaceWith($newPrice);
     });
     $('.three').on('click',function() {
         var currentTot = +$('#totPrice').text();
         var addedItem = +$('#staffPrice').text();
         var $newTotal = ((addedItem + currentTot));
-    var $newPrice = $('<span>' + $newTotal + '</span>');
+    var $newPrice = $('<span id="totPrice">' + $newTotal + '</span>');
     $('#totPrice').replaceWith($newPrice);
     });
     $('.four').on('click',function() {
-        var currentTot = +$('#totPrice').text();
-        var addedItem = +$('#evacPrice').text();
-        var $newTotal = ((addedItem + currentTot));
-    var $newPrice = $('<span>' + $newTotal + '</span>');
-    $('#totPrice').replaceWith($newPrice);
+            var currentTot = +$('#totPrice').text();
+            if(currentTot < 120 ) {
+            var addedItem = +$('#evacPrice').text();
+            var $newTotal = ((addedItem + currentTot));
+            var $newPrice = $('<span id="totPrice">' + $newTotal +'</span>');
+            $('#totPrice').replaceWith($newPrice);
+            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="firstCourse" value="firstCourse">'+'Evacation Training'+'</p>');
+        } else {
+            var currentTot = +$('#totPrice').text();
+            var addedItem = +$('#evacPrice').text();
+            var $newTotal = ((addedItem + currentTot) - 60);
+            var $disPrice = $('<span id="totPrice">' + $newTotal +'</span>');
+            var $saving = $('<h5>'+' You save $60 '+'</h5>');
+            $('#totPrice').replaceWith($disPrice);
+            $('#totPrice').append($saving);
+        }
     });
-
 });
 
