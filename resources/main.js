6530a49912735b490259921e73d6528b02bc9974
LyellScott <lyellscott@outlook.com>
2018-02-09 16:00:40 +1000

    Jquery almost completed

--- resources/main.js
+++ resources/main.js
@@ -3,26 +3,70 @@ $(document).ready (function() {
         $('form').removeClass("dimmerSwitch");
     });
     $('.one').on('click',function() {
-        var currentTot = +$('#totPrice').text();
-        var addedItem = +$('#extPrice').text();
-        var $newTotal = ((addedItem + currentTot));
-    var $newPrice = $('<span id="totPrice">' + $newTotal + '</span>');
-    $('#totPrice').replaceWith($newPrice);
+            var currentTot = +$('#totPrice').text();
+            if(currentTot < 120){
+            var addedItem = +$('#extPrice').text();
+            var $newTotal = ((addedItem + currentTot));
+            var $newPrice = $('<span id="totPrice">' + $newTotal + '</span>');
+            $('#totPrice').replaceWith($newPrice);
+            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="firstCourse" value="firstCourse">'+'Fire Extinguisher Training'+'</p>');
+        }else{
+            var currentTot = +$('#totPrice').text();
+            var addedItem = +$('#extPrice').text();
+            var $newTotal = ((addedItem + currentTot) - 100);
+            var $disPrice = $('<span id="totPrice">' + $newTotal + '</span>');
+            var $saving = $('<h5>'+' You save $100 '+'</h5>');
+            $('#totPrice').replaceWith($disPrice);
+            $('#totPrice').append($saving);
+            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="firstCourse" value="firstCourse">'+'Fire Extinguisher Training'+'</p>');
+        }
     });
+
+
     $('.two').on('click',function() {
-        var currentTot = +$('#totPrice').text();
-        var addedItem = +$('#wardenPrice').text();
-        var $newTotal = ((addedItem + currentTot));
-    var $newPrice = $('<span id="totPrice">' + $newTotal + '</span>');
-    $('#totPrice').replaceWith($newPrice);
+            var currentTot = +$('#totPrice').text();
+            if(currentTot < 120){
+            var addedItem = +$('#wardenPrice').text();
+            var $newTotal = ((addedItem + currentTot));
+            var $newPrice = $('<span id="totPrice">' + $newTotal + '</span>');
+            $('#totPrice').replaceWith($newPrice);
+            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="firstCourse" value="firstCourse">'+'Warden Training'+'</p>');
+        } else {
+            var currentTot = +$('#totPrice').text();
+            var addedItem = +$('#wardenPrice').text();
+            var $newTotal = ((addedItem + currentTot)-100);
+            var $disPrice = $('<span id="totPrice">' + $newTotal + '</span>');
+            var $saving = $('<h5>'+' You save $100 '+'</h5>');
+            $('#totPrice').replaceWith($disPrice);
+            $('#totPrice').append($saving);
+            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="firstCourse" value="firstCourse">'+'Warden Training'+'</p>');
+        }
     });
+
+
     $('.three').on('click',function() {
-        var currentTot = +$('#totPrice').text();
-        var addedItem = +$('#staffPrice').text();
-        var $newTotal = ((addedItem + currentTot));
-    var $newPrice = $('<span id="totPrice">' + $newTotal + '</span>');
-    $('#totPrice').replaceWith($newPrice);
+            var currentTot = +$('#totPrice').text();
+            if(currentTot < 120 ) {
+            var addedItem = +$('#staffPrice').text();
+            var $newTotal = ((addedItem + currentTot));
+            var $newPrice = $('<span id="totPrice">' + $newTotal + '</span>');
+            $('#totPrice').replaceWith($newPrice);
+            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="firstCourse" value="firstCourse">'+'General Staff Fire Training'+'</p>');
+        } else {
+            var currentTot = +$('#totPrice').text();    
+            var addedItem = +$('#staffPrice').text();
+            var $newTotal = ((addedItem + currentTot) - 100);
+            var $disPrice = $('<span id="totPrice">' + $newTotal + '</span>');
+            var $saving = $('<h5>'+' You save $100 '+'</h5>');
+            $('#totPrice').replaceWith($disPrice);
+            $('#totPrice').append($saving);
+            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="firstCourse" value="firstCourse">'+'General Staff Fire Training'+'</p>');
+        }
     });
+    
+    
+    
+    
     $('.four').on('click',function() {
             var currentTot = +$('#totPrice').text();
             if(currentTot < 120 ) {
@@ -39,6 +83,7 @@ $(document).ready (function() {
             var $saving = $('<h5>'+' You save $60 '+'</h5>');
             $('#totPrice').replaceWith($disPrice);
             $('#totPrice').append($saving);
+            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="firstCourse" value="firstCourse">'+'Evacation Training'+'</p>');
         }
     });
 });
