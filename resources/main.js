3124286dc8bbce45c61af8d7b0d9c4677a5fc8b4
LyellScott <lyellscott@outlook.com>
2018-02-15 07:06:34 +1000

    Completed Form

--- resources/main.js
+++ resources/main.js
@@ -2,6 +2,9 @@ $(document).ready (function() {
     $('.training-modules').on('click',function() {
         $('form').removeClass("dimmerSwitch");
     });
+    $('.dim').on('click',function() {
+        $('#extPrice h5').append('<h1>'+'This course has already been selected'+'</h1>');
+    });
     $('.one').on('click',function() {
             var currentTot = +$('#totPrice').text();
             if(currentTot < 120){
@@ -9,16 +12,25 @@ $(document).ready (function() {
             var $newTotal = ((addedItem + currentTot));
             var $newPrice = $('<span id="totPrice">' + $newTotal + '</span>');
             $('#totPrice').replaceWith($newPrice);
-            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="firstCourse" value="firstCourse">'+'Fire Extinguisher Training'+'</p>');
+            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="field8" value="Fire Extinguisher Training">'+'Fire Extinguisher Training'+'</p>');
+            $('html, body').animate({ scrollTop: 0 }, 'fast', function () {});
+            $(this).removeClass("one").addClass("dim");
         }else{
             var currentTot = +$('#totPrice').text();
             var addedItem = +$('#extPrice').text();
             var $newTotal = ((addedItem + currentTot) - 100);
             var $disPrice = $('<span id="totPrice">' + $newTotal + '</span>');
-            var $saving = $('<h5>'+' You save $100 '+'</h5>');
             $('#totPrice').replaceWith($disPrice);
-            $('#totPrice').append($saving);
-            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="firstCourse" value="firstCourse">'+'Fire Extinguisher Training'+'</p>');
+
+            var newSaving = +$('#discount').text();
+            var totSaving = (newSaving + 100);
+            var $disSaving = $('<span id="discount">'+ totSaving +'</span>');
+            var $youSaveOn = $('<span id="youSave">'+ 'You Save $' +'</span>');
+            $('#youSave').replaceWith($youSaveOn);
+            $('#discount').replaceWith($disSaving);
+            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="field8" value="Fire Extinguisher Training">'+'Fire Extinguisher Training'+'</p>');
+            $('html, body').animate({ scrollTop: 0 }, 'fast', function () {});
+            $(this).removeClass("one").addClass("dim");
         }
     });
 
@@ -30,16 +42,25 @@ $(document).ready (function() {
             var $newTotal = ((addedItem + currentTot));
             var $newPrice = $('<span id="totPrice">' + $newTotal + '</span>');
             $('#totPrice').replaceWith($newPrice);
-            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="firstCourse" value="firstCourse">'+'Warden Training'+'</p>');
+            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="field9" value="Warden Training">'+'Warden Training'+'</p>');
+            $('html, body').animate({ scrollTop: 0 }, 'fast', function () {});
+            $(this).removeClass("one").addClass("dim");
         } else {
             var currentTot = +$('#totPrice').text();
             var addedItem = +$('#wardenPrice').text();
             var $newTotal = ((addedItem + currentTot)-100);
             var $disPrice = $('<span id="totPrice">' + $newTotal + '</span>');
-            var $saving = $('<h5>'+' You save $100 '+'</h5>');
             $('#totPrice').replaceWith($disPrice);
-            $('#totPrice').append($saving);
-            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="firstCourse" value="firstCourse">'+'Warden Training'+'</p>');
+
+            var newSaving = +$('#discount').text();
+            var totSaving = (newSaving + 100);
+            var $disSaving = $('<span id="discount">'+ totSaving +'</span>');
+            var $youSaveOn = $('<span id="youSave">'+ 'You Save $' +'</span>');
+            $('#youSave').replaceWith($youSaveOn);
+            $('#discount').replaceWith($disSaving);
+            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="field9" value="Warden Training">'+'Warden Training'+'</p>');
+            $('html, body').animate({ scrollTop: 0 }, 'fast', function () {});
+            $(this).removeClass("one").addClass("dim");
         }
     });
 
@@ -51,16 +72,25 @@ $(document).ready (function() {
             var $newTotal = ((addedItem + currentTot));
             var $newPrice = $('<span id="totPrice">' + $newTotal + '</span>');
             $('#totPrice').replaceWith($newPrice);
-            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="firstCourse" value="firstCourse">'+'General Staff Fire Training'+'</p>');
+            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="field10" value="General Staff Fire Training">'+'General Staff Fire Training'+'</p>');
+            $('html, body').animate({ scrollTop: 0 }, 'fast', function () {});
+            $(this).removeClass("one").addClass("dim");
         } else {
             var currentTot = +$('#totPrice').text();    
             var addedItem = +$('#staffPrice').text();
             var $newTotal = ((addedItem + currentTot) - 100);
             var $disPrice = $('<span id="totPrice">' + $newTotal + '</span>');
-            var $saving = $('<h5>'+' You save $100 '+'</h5>');
             $('#totPrice').replaceWith($disPrice);
-            $('#totPrice').append($saving);
-            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="firstCourse" value="firstCourse">'+'General Staff Fire Training'+'</p>');
+            
+            var newSaving = +$('#discount').text();
+            var totSaving = (newSaving + 100);
+            var $disSaving = $('<span id="discount">'+ totSaving +'</span>');
+            var $youSaveOn = $('<span id="youSave">'+ 'You Save $' +'</span>');
+            $('#youSave').replaceWith($youSaveOn);
+            $('#discount').replaceWith($disSaving);
+            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="field10" value="General Staff Fire Training">'+'General Staff Fire Training'+'</p>');
+            $('html, body').animate({ scrollTop: 0 }, 'fast', function () {});
+            $(this).removeClass("one").addClass("dim");
         }
     });
     
@@ -74,16 +104,25 @@ $(document).ready (function() {
             var $newTotal = ((addedItem + currentTot));
             var $newPrice = $('<span id="totPrice">' + $newTotal +'</span>');
             $('#totPrice').replaceWith($newPrice);
-            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="firstCourse" value="firstCourse">'+'Evacation Training'+'</p>');
+            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="field11" value="Evacation Training">'+'Evacation Training'+'</p>');
+            $('html, body').animate({ scrollTop: 0 }, 'fast', function () {});
+            $(this).removeClass("one").addClass("dim");
         } else {
             var currentTot = +$('#totPrice').text();
             var addedItem = +$('#evacPrice').text();
             var $newTotal = ((addedItem + currentTot) - 60);
             var $disPrice = $('<span id="totPrice">' + $newTotal +'</span>');
-            var $saving = $('<h5>'+' You save $60 '+'</h5>');
             $('#totPrice').replaceWith($disPrice);
-            $('#totPrice').append($saving);
-            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="firstCourse" value="firstCourse">'+'Evacation Training'+'</p>');
+
+            var newSaving = +$('#discount').text();
+            var totSaving = (newSaving + 100);
+            var $disSaving = $('<span id="discount">'+ totSaving +'</span>');
+            var $youSaveOn = $('<span id="youSave">'+ 'You Save $' +'</span>');
+            $('#youSave').replaceWith($youSaveOn);
+            $('#discount').replaceWith($disSaving);
+            $('.selectCourses').append('<p>'+ '<input type="checkbox" checked="checked" name="field11" value="Evacation Training">'+'Evacation Training'+'</p>');
+            $('html, body').animate({ scrollTop: 0 }, 'fast', function () {});
+            $(this).removeClass("one").addClass("dim");
         }
     });
 });
